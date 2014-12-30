a47f8f95e80ffa829d8d260b0cf54587633c42c4
abdonwww <abdonwww@gmail.com>
2014-12-30 22:30:16 +0900

    Complete signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
-  before_save { self.email = email.downcase }
+  before_save {
+    email.downcase! # equal to self.email = email.downcase
+  }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
