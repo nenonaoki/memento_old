a5d8a39da55467460da1426d5bae6259216de74c
abdonwww <abdonwww@gmail.com>
2015-01-04 01:28:38 +0900

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
 
-  before_save {
-    self.email = email.downcase # equal to email.downcase!
-  }
+  has_secure_password # require "password_digest" column in database table
+  attr_accessor :remember_token, :activation_token
+
+  before_save :downcase_email # Invoked when created and updated
+  before_create :create_activation_digest # Invoked when created
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -16,9 +18,6 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 6 }, allow_blank: true
 
-  has_secure_password # require password_digest column in database table
-
-  attr_accessor :remember_token
 
 
   # Returns the hash digest of the given string. (Class method)
@@ -44,10 +43,34 @@ class User < ActiveRecord::Base
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil? # For multiple browers conflicts
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest") # self.send('method_name')
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
+  # Activates an account.
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase # equal to email.downcase!
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'users#new'
 
-  get 'sessions/new'
+  # get 'sessions/new'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   # get 'users/new'
   resources :users
+  resources :account_activations, only: [:edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
