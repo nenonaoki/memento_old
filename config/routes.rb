bd1149e616f3fb4c3bcdb4cb42b8b3c360a1e285
abdonwww <abdonwww@gmail.com>
2015-01-04 04:21:11 +0900

    Develop mailers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'users#new'
 
   # get 'sessions/new'
@@ -9,6 +13,7 @@ Rails.application.routes.draw do
   # get 'users/new'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
