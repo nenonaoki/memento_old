ac00ffa995f833d0fcdb1f0bd34994682b7f2235
abdonwww <abdonwww@gmail.com>
2015-02-03 23:49:19 +0900

    Change image renaming method

--- app/uploaders/poster_uploader.rb
+++ app/uploaders/poster_uploader.rb
@@ -45,8 +45,8 @@ class PosterUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "img.#{file.extension}" if original_filename
+  end
 
 end
